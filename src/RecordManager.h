c893d8296bba055a006c6f3182336711502e4560
DoctorKk <40132320+DoctorKk@users.noreply.github.com>
2020-06-26 22:44:52 +0800

    Add files via upload

---
+++ src/RecordManager.h
@@ -0,0 +1,47 @@
+/*
+ * @Author: 小文
+ * @Date: 2020-06-21 15:26:20
+ * @LastEditTime: 2020-06-26 22:43:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \新建文件夹\RecordManager.h
+ */ 
+#include"Attribute.h"
+#include"Condition.h"
+#include"minisql.h"
+#include"BufferManager.h"
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class RecordManager
+{
+public:
+    RecordManager() {}
+    BufferManager buffer;
+    
+    //obviously
+    int table_create(string tableName);
+    int table_drop(string tableName);
+
+    int index_drop(string indexName);
+    int index_create(string indexName);
+
+    int record_insert(string tableName, char *record, int recordSize);
+
+    //print all record of a table meet requirement
+    int record_showall(string tableName, vector<string> *attributeNameVector, vector<Condition> *conditionVector);
+    
+    //delete all record of a table meet requirement
+    int record_deleteall(string tableName, vector<Condition> *conditionVector);
+    
+
+    //int indexRecordAllAlreadyInsert(string tableName, string indexName);
+
+private:
+    
+    int record_blockshow(string tableName, vector<string> *attributeNameVector, vector<Condition> *conditionVector, Block* block);
+    int record_blockdelete(string tableName, vector<Condition> *conditionVector, int blockOffset);
+};
+
