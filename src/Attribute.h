0b1e6035b08d7a092a0644e24eb5c9c2a4ffff8a
Holly <60909316+Hollie7@users.noreply.github.com>
2020-07-01 15:33:45 +0800

    Update Attribute.h

--- src/Attribute.h
+++ src/Attribute.h
@@ -1,16 +1,6 @@
-/*
- * @Author: 小文
- * @Date: 2020-06-24 16:13:35
- * @LastEditTime: 2020-06-24 17:42:35
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \新建文件夹\Attribute.h
- */
-
-#ifndef ATTRIBUTE_H
+#ifndef ATTRIBUTE_H 
 #define ATTRIBUTE_H
-#include <string>
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 class Attribute
@@ -21,15 +11,29 @@ public:
     string name;
     int type, size;              //-1为float，0为int，其余整数对应varchar(type)    
     bool ifUnique;          //1为unique, 0不unique
-    string index;    
-         
-    Attribute(string n, int t, bool i){
-        name = n;
-        type = t; 
-        ifUnique = i;
-        index = "";
-    }
-    ~Attribute(){};
-};
+    string index;
 
-#endif
+    Attribute(string n, int t, bool i, string in = "") {
+        name = n;
+        type = t;
+        ifUnique = i;
+        index = in;
+    }
+    string getName() {
+        return name;
+    }
+    int getType() {
+        return type;
+    }
+    bool getUnique() {
+        return ifUnique;
+    }
+    string getIndex() {
+        return index;
+    }
+    void print()
+    {
+        cout << "name:" << name << ";type: " << type << ";ifUnique: " << ifUnique << endl;
+    }
+    ~Attribute() {};
+};
