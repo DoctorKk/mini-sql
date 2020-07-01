63bf08c646e73a9a70e40d5b64f8627704c35cbf
Alex_Hwang <3180102081@zju.edu.cn>
2020-07-01 14:10:35 +0800

    for testing

---
+++ src/testing.cpp
@@ -0,0 +1,11 @@
+#include"minisql.h"
+#include"BufferManager.h"
+#include"CatalogManager.h"
+#include"RecordManager.h"
+
+using namespace std;
+
+int main() {
+    BufferManager bm;
+    RecordManager rm;
+}
