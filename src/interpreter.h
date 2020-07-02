8f22b6c4e0d2bd9d8a73705147a733b8a4482f69
fuzhouyuyue <66128076+fuzhouyuyue@users.noreply.github.com>
2020-07-02 23:11:16 +0800

    Update interpreter.h

--- src/interpreter.h
+++ src/interpreter.h
@@ -15,12 +15,26 @@ public:
     API* ap;
 	string fileName ;
 	int interpreter(string s);	
-    int EXEC_CREATE(string s,int *tmp,string word)
-    int EXEC_CREATE_TABLE(string s,int *tmp,string word)
-    int EXEC_CREATE_INDEX(string s,int *tmp,string word)
-    int EXEC_SELECT(string s,int *tmp,string word)
-    int EXEC_INSERT(string s,int *tmp,string word)
-	string getWord(string s, int *st);
+    int EXEC_CREATE(string s,int *tmp,string word);
+    //Create Table
+    int EXEC_CREATE_TABLE(string s,int *tmp,string word);
+    //Create Index
+    int EXEC_CREATE_INDEX(string s,int *tmp,string word);
+    //Drop 
+    int EXEC_DROP(string s,int *tmp,string word);
+    //Drop Table
+    int EXEC_DROP_TABLE(string s,int *tmp,string word); 
+    //Drop Index
+    int EXEC_DROP_INDEX(string s,int *tmp,string word);
+    //Select
+    int EXEC_SELECT(string s,int *tmp,string word);
+    //Insert
+    int EXEC_INSERT(string s,int *tmp,string word);
+    //Quit
+    int EXEC_QUIT(string s,int *tmp,string word);
+    //Execfile 
+    int EXEC_EXECFILE(string s, int *tmp,string word);
+    string getWord(string s, int *tmp);
 	
 	Interpreter(){}
 	~Interpreter(){}
