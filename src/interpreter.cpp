2c7f759332ab48dae7ee4ba57c3c7dea86383788
fuzhouyuyue <66128076+fuzhouyuyue@users.noreply.github.com>
2020-07-03 20:58:03 +0800

    Update interpreter.cpp

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -476,7 +476,7 @@ string Interpreter::getWord(string s, int *tmp)
 	string word;
 	int idx1,idx2;
 
-	while ((s[*tmp] == ' ' || s[*tmp] == 10  || s[*tmp] == '\t') && s[*tmp] != 0)
+	while ((s[*tmp] == ' '|| s[*tmp] != '\''  || s[*tmp] == 10  || s[*tmp] == '\t') && s[*tmp] != 0)
 	{
 		(*tmp)++;
 	}
@@ -510,7 +510,7 @@ string Interpreter::getWord(string s, int *tmp)
 	}
 	else
 	{
-		while (s[*tmp] != ' ' &&s[*tmp] != '(' && s[*tmp] != 10 && s[*tmp] != 0 && s[*tmp] != ')' && s[*tmp] != ',')
+		while (s[*tmp] != ' '&&s[*tmp] != '\''&&s[*tmp] != '(' && s[*tmp] != 10 && s[*tmp] != 0 && s[*tmp] != ')' && s[*tmp] != ',')
 			(*tmp)++;
 		idx2 = *tmp;
 		if (idx1 != idx2)
